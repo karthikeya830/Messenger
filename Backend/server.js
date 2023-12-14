dad78b5d6501430c8cbbd5789b258fba57a65aa1
Karthikeya Kayithoju <kayithojukarthikeya@gmail.com>
2023-12-14 19:31:52 +0530

    Created a basic backend server

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.get( '/', (req, res) => {
+    res.send(`Hello iam starting messenger application`)
+} )
 
 
 app.listen( 5000 , 'listening at port 5000' );
